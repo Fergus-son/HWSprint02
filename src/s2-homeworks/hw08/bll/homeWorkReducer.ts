0cbd1cea14e09c881594b42ab3230d1fa446bd60
Fergus-son <sarapovnikolaj790@gmail.com>
2025-10-16 18:21:19 +0700

    сделал HW08 1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -10,13 +10,14 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[] = initialState, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
+            const newState = [...state]
             if (action.payload === 'up') {
-                state.sort((a, b) => a.name.localeCompare(b.name))
+                newState.sort((a, b) => a.name.localeCompare(b.name))
             } else {
                 // (action.payload === 'down')
-                state.sort((a, b) => b.name.localeCompare(a.name))
+                newState.sort((a, b) => b.name.localeCompare(a.name))
             }
-            return state
+            return newState
         }
         case 'check': {
             return state.filter(a => a.age >= 18)
