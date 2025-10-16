c47cebc0767d842e2f7e2a301c84e6d72e44a50e
Fergus-son <sarapovnikolaj790@gmail.com>
2025-10-16 17:54:31 +0700

    сделал HW08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
+
+const initialState: UserType[] = []
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
 
-            return state // need to fix
+export const homeWorkReducer = (state: UserType[] = initialState, action: ActionType): UserType[] => { // need to fix any
+    switch (action.type) {
+        case 'sort': {
+            if (action.payload === 'up') {
+                state.sort((a, b) => a.name.localeCompare(b.name))
+            } else {
+                // (action.payload === 'down')
+                state.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(a => a.age >= 18)
+            // return state // need to fix
         }
         default:
             return state
